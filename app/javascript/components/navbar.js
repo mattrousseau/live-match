c0fa0d491be9271e610214b2c3fcd7887c9d43cc
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-12-07 16:03:23 +0100

    changed hp assets

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,13 +3,17 @@ const banner = document.querySelector('.banner');
 
   const initUpdateNavbarOnScroll = () => {
     const navbar = document.querySelector('.navbar-wagon');
+    const brand = document.querySelector('.navbar-wagon-brand');
     if (banner) {
       if (navbar) {
+        brand.classList.add('small');
         window.addEventListener('scroll', () => {
           if (window.scrollY >= window.innerHeight * 0.7) {
             navbar.classList.add('navbar-wagon-white');
+            brand.classList.remove('small');
           } else {
             navbar.classList.remove('navbar-wagon-white');
+            brand.classList.add('small');
           }
         });
       }
